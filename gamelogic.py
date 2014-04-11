4827e03e53145cc419c8bad290ccd15adbf29538
ana0 <sarah.anne.friend@gmail.com>
2014-04-11 12:26:41 -0400

    not broken!

--- gamelogic.py
+++ gamelogic.py
@@ -1,37 +1,26 @@
 class Sequence(object):
     
-    def __init__(self, query, **answers):
+    def __init__(self, query, *answers):
         self.query = query
         self.answers = answers
 
     def game_play(self):
         print(self.query)
         tries = 0
-        as_list = self.answers.keys()
+        as_list = list(self.answers)
         answer = input(" . . . ?").strip()
         while tries <= 2:
-            if answer in as_list:
-                print(self.answers[answer])
+            if int(answer) in range(1, len(as_list)):
+                print(as_list[int(answer)-1])
                 break
             else:
                 print("You speak to me like I can understand you. Try again")
                 tries += 1
                 answer = input(" . . . ?").strip()
         else:
-            while answer not in as_list:
+            while int(answer) not in range(1, len(as_list)):
                 print("Imagine what language sounds like to a rock.")
                 answer = input(" . . . ?").strip()
             else:
-                print(self.answers[answer])
+                print(as_list[int(answer)])
                     
-opening_sequence = Sequence("Around you there is only darkness -- darkness extending " 
-                            "in every direction, thick as a mattress and heavy as an ending. " 
-                            "You can feel its weight on you.\n\nDo you open your eyes?\n " 
-                            "    1 - Yes\n     2 - No", one = "Darkness is the absence of light.  I " 
-                            "describe what you perceive as darkness because there is no light down here, " 
-                            "but you do not see it as black.\nIt is all you've ever known, " 
-                            "the only colour.  Indeed, you don\'t really \"see\" at all.\n" 
-                            "How are you so sure you have eyes?\nYou are a rock.\n", two = "Yes, what is there " 
-                            "to be curious about, anyway.\nAfter all -- you are a rock.\n")
-
-opening_sequence.game_play()
