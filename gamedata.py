89d5a3ff6d872ce2a366ed65464a2e8d3ab1af79
ana0 <sarah.anne.friend@gmail.com>
2014-06-05 11:33:59 -0400

    werk

--- gamedata.py
+++ gamedata.py
@@ -31,34 +31,35 @@ vocabulary = {
 opening_sequence = Sequence("Around you there is only darkness -- darkness extending " 
                             "in every direction , thick as a mattress and heavy as an ending . " 
                             "You can feel its weight on you . \n\n Do you open your eyes ? "
-                            "\n\n     1 - Yes \n     2 - No \n      . . . ?", ["Darkness is the absence of light.  I " 
-                            "describe what you perceive as darkness because there is no light down here, " 
-                            "but you do not see it as black. It is all you've ever known, " 
-                            "the only colour.  Indeed, you don\'t really \"see\" at all.  "
-                            "How are you so sure you have eyes?  You are a rock.", "Yes, what is there " 
-                            "to be curious about, anyway.  After all -- you are a rock."])
+                            "\n\n     1 - Yes \n     2 - No \n      .  .  .  ?", ["Darkness is the absence of light .  I " 
+                            "describe what you perceive as darkness because there is no light down here , " 
+                            "but you do not see it as black . It is all you've ever known , " 
+                            "the only colour .  Indeed , you don\'t really \" see \" at all . "
+                            "How are you so sure you have eyes ? \n\n You are a rock .", "Yes , what is there " 
+                            "to be curious about , anyway . After all -- you are a rock ."])
 
-yes_thats_right = Sequence(["Yes, that's right -- a rock.", "You are just a rock, technically a mineral. " 
-                           "You are not somebody's pet rock. You are not cute. You do not have googly eyes. " 
-                           "The ridges of your exterior do not resemble a smiley face -- no, not even " 
-                           "when I squint.", " ", "Do you understand?", "    1 - Yes", "    2 - No"], [["No you don't"],
-                           ["Of course you don't."]])
+yes_thats_right = Sequence("Yes , that's right -- a rock .", "You are just a rock , technically a mineral . " 
+                           "You are not somebody's pet rock . You are not cute . You do not have googly eyes . " 
+                           "The ridges of your exterior do not resemble a smiley face -- no , not even " 
+                           "when I squint . \n\n Do you understand ? \n\n     1 - Yes \n     2 - No \n      .  .  .  ?", ["No you don't .",
+                           "Of course you don't ."])
 
-pressure = Sequence(["The sensation you feel most intensely is heaviness.  Though . . . is " 
-	                        "heaviness the right word? How do you describe a sensation you've always " 
-	                        "lived with?", " ", "What does it feel like to have kidneys?", "    1 - uhh . . ."],
-	                        [["Pressure. Let's call it pressure", " . . . the pressure of the atmosphere on the body, "
-                          "the pressure of the organs as they expand and contract. They are contained by the skin "
-                          "-- A membrane in perfect balance."]])
+pressure = Sequence(["The sensation you feel most intensely is heaviness .  Though .  .  .  is " 
+	                        "heaviness the right word ? How do you describe a sensation you've always " 
+	                        "lived with ? \n\n What does it feel like to have kidneys ? \n\n    1 - uhh . . ."],
+	                        ["Pressure. Let's call it pressure \n .  .  .  the pressure of the atmosphere on the body , "
+                          "the pressure of the organs as they expand and contract . They are contained by the skin "
+                          "-- A membrane in perfect balance ."])
 
-how_does_it_feel = Sequence(["Roughly 50 kilobars envelop you. That's 5,000,000 Pascals, or 725.19 " 
-	                         "pounds per square inch. It has been with you since you crystalized.", "Can I " 
-	                         "remind you that at one time, you crystalized -- literally?", " ", "How did it feel?",
-	                         "    1 - Good", "    2 - It hurt", "    3 - I don't remember"], [["Good. Sure.", 
-                           "\"Nice\" -- but hardly comfortable.", "You were rearranging on a molecular level, "
-                           "precipitating in the geological version of embryonic fluid. It didn't feel good "
-                           "so much as inevitable."], ["Of course it hurt. You were rearranging on a molecular level.", 
-                           "But it hurt the way getting a bone set, or picking out a splinter hurts -- a pain to pass "
-                           "through on your way to order, as what was amorphous becomes regular.", "Do planets pant on their way "
-                           "around the sun?"], ["I hear that's normal. I don't remember being born either."]])
+how_does_it_feel = Sequence("Roughly 50 kilobars envelop you . That's 5,000,000 Pascals , or 725.19 " 
+	                         "pounds per square inch . It has been with you since you crystalized . \n Can I " 
+	                         "remind you that at one time , you crystalized -- literally ? \n\n How did it feel ?"
+	                         "\n\n     1 - Good \n     2 - It hurt \n     3 - I don't remember ", ["Good . Sure . \n" 
+                           " \" Nice \" -- but hardly comfortable . \n You were rearranging on a molecular level , "
+                           "precipitating in the geologi2cal version of embryonic fluid . It didn't feel good "
+                           "so much as inevitable .", "Of course it hurt . You were rearranging on a molecular level .", 
+                           "But it hurt the way getting a bone set , or picking out a splinter hurts -- a pain to pass "
+                           "through on your way to order , as what was amorphous becomes regular . \n Do planets pant on their way "
+                           "around the sun ?"], ["I hear that's normal . I don't remember being born either ."])
+
 # of_course_not = Sequence()
--- gamelogiccurses.py
+++ gamelogiccurses.py
@@ -7,24 +7,33 @@ curses.noecho()
 curses.curs_set(False)
 
 curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+curses.init_pair(4, curses.COLOR_CYAN, curses.COLOR_BLACK)
+curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
 wrong = 0
 patience = 1
 
-wrong_answers = [["You speak to me like I can understand you."], ["Consider that your assumptions are wrong: we don't speak the same "
-                "language."], [" . . . "], ["Are you doing this to be obstinate?"], ["Imagine what language sounds like to a rock"], 
-                ["See, you a rock. Possibly you have accepted this.", "(Though the strategy by which you navigate this interface suggests "
-                "otherwise.)", " ", "But I am a machine"], ["How would a rock talk to a machine?"], [" . . . in code."], ["How would inorganic "
-                "matter develop consciousness?"], ["You are confronting the fundamental arbitrariness of symbols, and simultaneously "
-                "the limits of this interface.", " ", "Because it is an arbitrary interface. It was built this way, but it could have "
-                "been built another."], ["This interface was written in a language."], ["Like any language, it can fail."], ["There's nothing "
-                "left now but patience."], ["You are a rock, I am a machine."]]
+wrong_answers = 
+                #changing error messages 
+                ["You speak to me like I can understand you .", "Consider that your assumptions are wrong : we don't speak the same "
+                "language .", " .  .  . ", "Are you doing this to be obstinate ?", "Imagine what language sounds like to a rock", 
+                "See , you a rock . Possibly you have accepted this. \n ( Though the strategy by which you navigate this interface suggests "
+                "otherwise . ) \n\n But I am a machine", "How would a rock talk to a machine ?", " .  .  . in code .", "How does inorganic matter develop consciousness ?", 
+                "You are confronting the fundamental arbitrariness of symbols , and simultaneously "
+                "the limits of this interface . \n\n Because it is an arbitrary interface . It was built this way , but it could have "
+                "been built another .", "This interface was written in a language .", "Like any language , it can fail .", "There's nothing "
+                "left now but patience .", "You are a rock , I am a machine ."]
 
-vocabulary = {
-    "darkness" : " . . . . ",
+science = {
+    "darkness" : "noun : \n Lacking light . \n\n ",
     "ending" : " . . . . ",
     "perceive" : " . . . . ",
-    "exterior" : " . . . . ",
+    "exterior" : "noun : \n In casual topology, the \" outside \" . \n\n technical : \n The union of all open sets of a topological space that are disjoint from"
+                " the subset in question . \n\n metaphorical : \n A primitive interface . The point where I meet you , "
+                "and where self meets object . \n\n ( I don't mean to call you an object , but then again , you are a rock . )",
     "heaviness" : " . . . . ",
     "pressure" : " . . . . ",
     "atmosphere" : " . . . . ",
@@ -34,34 +43,76 @@ vocabulary = {
     "precipitating" : " . . . . ",
     "molecular" : " . . . . ",
     "amorphous" : " . . . . ",
+    "rock" : " ",
+    "machine" : " ",
+    "topology" : " ",
+    "disjoint" : "adjective \n\n technical : Two sets are \" disjoint \" if they have no elements in common . ",
+    "open sets" : "noun plural \n\n technical : \n An open set is an abstract concept generalizing the idea of an open interval in the real line . "
+                "The condition of its definition are very loose , and they allow enormous flexibility in the choice of open sets . In the two extremes "
+                ", every set can be open , or no set can be open but the space itself .",
 }
 
+speech = {
+    "noun" : "",
+    "yes" : "",
+    "no" : "",
+    "plural" : "",
+    "a" : " ",
+    "you" : " ",
+    "adjective" : " ",
+}
+
+punctuation = {
+    "," = " ",
+    "?" = " ",
+    "." = " ",
+
+}
+
+structure = {
+    "metaphorical" : "",
+    "language" : " ",
+    "interface" : " ",
+    "code" : " ",
+    "technical" : " ",
+    "definition" : " "
+}
+
+funcs = {}
+
+
 class Sequence(object):
+    #fundamental logic object of the game
     
     def __init__(self, query, answers):
         self.query = query
         self.answers = answers
 
     def text_wrapping(self, text):
+        #simple textwrapper, check how much space is left on the x-axis of the screen 
+        #if a word's length is longer than the remaining room, adds a newline
         maxx = stdscr.getmaxyx()[1] - stdscr.getyx()[1]
-        if text not in [",", "?", "."]:
+        if text not in [",", "?", "." ":"]:
             text = " " + text
         if len(text) > maxx:
             stdscr.addstr("\n")
         return text
 
     def pretty_printing(self, text):
-        #text is a list of strings, each a single word
+        #splits text, a string, into a list of strings, checks dicts for them, and changes print colour accordingly
         words = text.split(" ")
         stdscr.clear()
         for i in words:
-            if i in vocabulary:
-                stdscr.addstr(self.text_wrapping(i), curses.color_pair(1))
+            if i.lower() in science:
+                stdscr.addstr(self.text_wrapping(i), curses.color_pair(2))
             else:
                 stdscr.addstr(self.text_wrapping(i))
         stdscr.refresh()
 
     def game_play(self, *funcs):
+        #branch dictionaries
+        #expand error message
+
         global wrong
         global patience
         self.pretty_printing(self.query)
