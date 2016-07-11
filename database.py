3b5f13cc15701edaea5c6df0a607013a8f1e6519
ana0 <sarah.anne.friend@gmail.com>
2016-07-11 11:53:15 -0400

    Add more nodes

--- database.py
+++ database.py
@@ -122,6 +122,76 @@ twenty8 = GameNode(28, [" . . . enough.", "I hope you found that instructive.",
     "Would you like to continue as a rock?"],
     ["0 - Yes", "1 - No"])
 
+twenty9 = GameNode(29, ["The sensation you feel most intensely is heaviness."
+    " Though  .  .  .  is heaviness the right word? How do you describe a "
+    "sensation you've always lived with?", "What does it feel like to have "
+    "kidneys ? "],
+    ["0 - . . . uhh", "1 - I never thought about it", "2 - It's painful"])
+
+thirty = NoAnswerNode(30, ["Maybe you should."])
+
+thirty1 = NoAnswerNode(31, ["Yes, pain is universal."])
+
+thirty2 = NoAnswerNode(32, ["Pressure. Let's call it pressure", ".  .  .  the "
+    "pressure of the atmosphere on the body, the pressure of the organs as they"
+    " expand and contract. They are contained by the skin -- A membrane in "
+    "perfect balance."])
+
+thirty3 = GameNode(33, ["Roughly 16,500,000 Pascals envelopes you -- that's "
+    "2,393.12 pounds per square inch, or 162.84 times the pressure of the "
+    "atmosphere at sea level. It has been with you since you crystalized.", 
+    "Can I remind you that at one time, you crystalized -- literally?", "How "
+    "did it feel?"],
+    ["0 - It felt good", "1 - It hurt", "2 - I don't remember"])
+
+thirty4 = GameNode(34, ["Of course it hurt. You were rearranging on a "
+    "molecular level.", "How much did it hurt?"],
+    ["0 - It's not so bad", "1 - The worst pain I've ever felt", 
+    "2 - I don't have words to describe it"])
+
+thirty5 = NoAnswerNode(35, ["Good. Sure.", "'Nice' -- but hardly comfortable.",
+    "You were rearranging on a molecular level, precipitating in the geological"
+    " version of embryonic fluid. It didn't feel good so much as inevitable.", 
+    "It also felt hot. Over time, you will become very good at resisting "
+    "heat."])
+
+thirty6 = NoAnswerNode(36, ["I hear that's normal. I don't remember being born "
+    "either."])
+
+thirty7 = GameNode(37, ["Words are useless anyway", "What about numbers?"],
+    ["0 - i", "1 - 96485.33289(59) C mol−1", "2 - 4.669201 . . .", "3 - 0"])
+
+thirty8 = GameNode(38, ["It hurt the way getting a bone set, or picking out a "
+    "splinter hurts -- a pain to pass through on your way to order, as what was"
+    " amorphous becomes regular.", "It also felt hot.  Over time, you will "
+    "become very good at resisting heat."])
+
+thirty9 = NoAnswerNode(39, ["Sorry to hear that.", "There's nothing you can do "
+    "to escape it."])
+
+forty = NoAnswerNode(40, ["You feel the particles that make up your body hum "
+    "like electric honeybees, a frenetic whir that stings you from all "
+    "directions.", "Every single part of you is moving all the time and yet you"
+    " are immobile."])
+
+forty1 = NoAnswerNode(41, ["You listen to darkness around you . . .", "You hear"
+    " a powerful engine on the earth's surface far above - or perhaps it's the "
+    "roar of a cooling fan. Try and remember the loudest thing you've ever "
+    "heard."])
+
+forty2 = NoAnswerNode(42, ["You listen to the darkness around you.", "You feel "
+    "a vibration that represents a sound: it's a grain of sand settling against"
+    " another far above you on the earth's surface, rippling down. Try and "
+    "remember the quietest thing you've ever heard."])
+
+forty3 = NoAnswerNode(43, ["You listen to the darkness around you, but you hear"
+    " nothing at all.", "Imagine the complete absence of sound."])
+
+forty4 = GameNode(44, ["So, how long have you been down here?"],
+    ["0 - 1.7060976e+16 seconds", "1 - As long as the lifespans of 154,571,428 "
+    "bees , or 7,213,333 humans", "2 - 25.51 percent of the estimated age of "
+    "the universe", "3 - Since the end of the Precambrian Eon"])
+
 opening.answer_map = {"0":one, "1":two, "yes":one, "no":two, "y":one, "n":two}
 one.answer_map = {"0":eight, "1":eight, "2":eight}
 two.answer_map = {"0":four, "1":five, "2":six, "3":three, "4":nine}
@@ -139,15 +209,32 @@ ten3.answer_map = {"0":ten6}
 ten4.answer_map = {"0":ten6}
 ten5.answer_map = {"0":seven}
 ten6.answer_map = {"0":ten7, "1":ten8, "2":ten9, "3":twenty}
-ten7.answer_map = {"0":""}
-ten8.answer_map = {"0":""}
-ten9.answer_map = {"0":""}
+ten7.answer_map = {"0":twenty9}
+ten8.answer_map = {"0":twenty9}
+ten9.answer_map = {"0":twenty9}
 twenty.answer_map = {"0":twenty1, "1":twenty2, "2":twenty1}
 twenty1.answer_map = {"0":twenty6, "1":twenty6, "2":twenty6, "3":twenty6}
 twenty2.answer_map = {"0":twenty4, "1":twenty5, "2":twenty3}
-twenty3.answer_map = {"0":""}
-twenty4.answer_map = {"0":""}
-twenty5.answer_map = {"0":""}
+twenty3.answer_map = {"0":twenty9}
+twenty4.answer_map = {"0":twenty9}
+twenty5.answer_map = {"0":twenty9}
 twenty6.answer_map = {"0":twenty7, "1":twenty7, "2":twenty7 "3":twenty7}
-twenty7.answer_map = {"0":"", "1":"", "2":"", "3":""}
-twenty8.answer_map = {"0":"", "1":}
+twenty7.answer_map = {"0":twenty8, "1":twenty8, "2":twenty8, "3":twenty8}
+twenty8.answer_map = {"0":twenty9, "1":seven}
+twenty9.answer_map = {"0":thirty2, "1":thirty, "2":thirty1}
+thirty.answer_map = {"0":thirty2}
+thirty1.answer_map = {"0":thirty2}
+thirty2.answer_map = {"0":thirty3}
+thirty3.answer_map = {"0":thirty5, "1":thirty4, "2":thirty6}
+thirty4.answer_map = {"0":thirty8, "1":thirty9, "2":thirty7}
+thirty5.answer_map = {"0":forty4}
+thirty6.answer_map = {"0":forty4}
+thirty7.answer_map = {"0":forty1, "1":forty, "2":forty2 "3":""}
+thirty8.answer_map = {"0":forty4}
+thirty9.answer_map = {"0":forty4}
+forty.answer_map = {"0":forty4}
+forty1.answer_map = {"0":forty4}
+forty2.answer_map = {"0":forty4}
+forty3.answer_map = {"0":forty4}
+forty4.answer_map = {"0":"", "1":"", "2":"", "3":""}
+
