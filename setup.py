f0c609c281a55d523d68ac1f7bb5c389dad807f4
ana0 <sarah.anne.friend@gmail.com>
2016-09-25 16:39:00 -0400

    Update build file for icon bug

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['main.py'])
+setup(console=['main.py'])from distutils.core import setup
+import py2exe
+
+# setup(console=[{
+#             "script": "main.py",                    ### Main Python script    
+#             "icon_resources": [(1, "icon.ico")]     ### Icon to embed into the PE file.
+#         }])
+
+setup_dict = dict(
+    console = [{'script': "youarearock.py",
+                "icon_resources": [(1, "icon.ico")]}],
+)
+
+setup(**setup_dict)
+setup(**setup_dict)
